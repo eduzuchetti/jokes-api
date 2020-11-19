b8c67729e6f394b255ef8345846e42bdd703a97c
Eduardo Zuchetti <eduzuchetti@gmail.com>
2020-11-18 22:28:07 -0300

    fix(logging) FIx log level and messages

--- src/controllers/jokes.ts
+++ src/controllers/jokes.ts
@@ -15,8 +15,8 @@ const defaultResponse = (
     error: Error | null,
     data: Document | Document[] | null) => {
         if (!(error) && !(data)) {
-            console.error('[ERROR] JokesController.defaultResponse: No ' +
-                '"Error" ou "Data" was passed to default response')
+            console.error('JokesController.defaultResponse: Any "error"' +
+                ' or "data" was passed to generate default response')
         } else if (error) {
             return {
                 status: http_code,
@@ -33,15 +33,13 @@ const defaultResponse = (
 }
 
 const GetAll = (req: Request, res: Response, next: NextFunction) => {
-    console.debug('[DEBUG] JokesController.GetAll: function started.')
+    console.debug('JokesController.GetAll: function started.')
     
     JokesModel.find({}).limit(10).then(result => {
-        console.debug('[DEBUG] JokesController.GetAll: Query find() success. Data:', result)
-        
+        console.debug('JokesController.GetAll: Query find() success. Data:', result)
         return res.status(200).json(defaultResponse(200, null, result))
-        
     }).catch(err => {
-        console.error('[DEBUG] JokesController.GetAll: Query find() failed with error')
+        console.error('JokesController.GetAll: Query find() failed with error')
         return res.status(500).json(defaultResponse(500, err, null))
     })
 }
@@ -52,15 +50,15 @@ const GetRandom = (req: Request, res: Response, next: NextFunction) => {
 
         JokesModel.findOne().skip(random).then(result => {
             if (result) {
-                console.log('[DEBUG] JokesController.GetRandom: Found random Joke:', result)
+                console.debug('JokesController.GetRandom: Found random Joke:', result)
                 return res.status(200).json(defaultResponse(200, null, result))
             } else {
                 let err = Error('Can\'t find random Joke: Null response from DB')
-                console.error('[ERROR] JokesController.GetOne:', err.message)
+                console.debug('JokesController.GetOne:', err.message)
                 return res.status(404).json(defaultResponse(404, err, null))
             }
         }).catch(err => {
-            console.error('[ERROR] JokesController.GetRandom: Failed to execute query findOne().skip(random):', err)
+            console.error('JokesController.GetRandom: Failed to execute query findOne().skip(random):', err)
             return res.status(500).json(defaultResponse(500, err, null))
         })
     })
@@ -70,20 +68,20 @@ const GetOne = (req: Request, res: Response, next: NextFunction) => {
     let jokeid: String = req.params.jokeid
 
     if (!jokeid) {
-        console.log('[INFO] JokesController.GetOne: Any id was passed:', jokeid)
+        console.warn('JokesController.GetOne: Any id was passed:', jokeid)
         return res.status(400).json(defaultResponse(404, Error('Must pass an Id'), null))
     }
 
     JokesModel.findOne({ _id: jokeid }).then(result => {
         if (result) {
-            console.log('[INFO] JokesController.GetOne: Found joke with Id', jokeid, ': ', result)
+            console.info('JokesController.GetOne: Found joke with Id', jokeid, ': ', result)
             return res.status(200).json(defaultResponse(200, null, result))
         } else {
-            console.error('[ERROR] JokesController.GetOne: Joke not found with id' + jokeid)
+            console.info('JokesController.GetOne: Joke not found with id' + jokeid)
             return res.status(404).json(defaultResponse(404, Error(`Joke not found with id ${jokeid}`), null))
         }
     }).catch(err => {
-        console.error('[ERROR] JokesController.GetOne: Failed to execute query FindOne() with JokeID', jokeid)
+        console.error('JokesController.GetOne: Failed to execute query FindOne() with JokeID', jokeid)
         return res.status(500).json(defaultResponse(500, err, null))
     })
 }
@@ -92,8 +90,8 @@ const Create = (req: Request, res: Response, next: NextFunction) => {
     let body = req.body
 
     if (!body) {
-        let err = Error('[INFO] JokesController.Create: Body can\'t be null. Passed: ' + body)
-        console.log(err.message)
+        let err = Error('JokesController.Create: Body can\'t be null. Passed: ' + body)
+        console.warn(err.message)
         return res.status(400).json(defaultResponse(400, err, null))
     }
 
@@ -102,14 +100,13 @@ const Create = (req: Request, res: Response, next: NextFunction) => {
         answer: body.answer,
         mainTheme: body.themes,
         author: []
-    })    
+    })
 
     newJoke.save().then(result => {
-        console.log('[INFO] JokesController.Create: Joke saved with success:', result)
+        console.info('JokesController.Create: Joke saved with success:', result)
         return res.status(201).send(result)
-        
     }).catch(err => {
-        let custom_err = Error('[ERROR] JokesController.Create: Fail to save the new Joke:' + err)
+        let custom_err = Error('JokesController.Create: Fail to save the new Joke:' + err)
         console.error(custom_err.message)
         return res.status(500).json(defaultResponse(500, err, null))
     })
@@ -119,28 +116,22 @@ const Delete = (req: Request, res: Response, next: NextFunction) => {
     let jokeid: String = req.params.jokeid
 
     if (!jokeid) {
-        console.log('[INFO] JokesController.Delete: Any id was passed:', jokeid)
+        console.warn('JokesController.Delete: Any id was passed:', jokeid)
         return res.status(400).json(defaultResponse(404, Error('Must pass an Id'), null))
     }
 
     JokesModel.findByIdAndDelete({ _id: jokeid }).then(result => {
         if (result) {
-            console.log('[INFO] JokesController.Delete: Found and Deleted joke with Id', jokeid, ': ', result)
+            console.info('JokesController.Delete: Found and Deleted joke with Id', jokeid, ': ', result)
             return res.status(200).json(defaultResponse(200, null, result))
         } else {
-            console.error('[ERROR] JokesController.Delete: Joke not found with id' + jokeid)
-            return res.status(404).json(defaultResponse(404, Error(`Joke not found with id ${jokeid}`), null))
+            console.info('JokesController.Delete: Joke not found with Id' + jokeid)
+            return res.status(404).json(defaultResponse(404, Error(`Joke not found with Id ${jokeid}`), null))
         }
     }).catch(err => {
-        console.error('[ERROR] JokesController.Delete: Failed to execute query findByIdAndDelete() with JokeID', jokeid)
+        console.error('JokesController.Delete: Failed to execute query findByIdAndDelete() with JokeID', jokeid)
         return res.status(500).json(defaultResponse(500, err, null))
     })
-}
-
-const _ChangeStatus = (status: String, value: Boolean, jokeid: String) => {
-    if (status && value && jokeid) {
-        
-    }
 }
 
 export default {
